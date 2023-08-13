e2feda0297ebf08c987f6420c39b2f634bc5b012
Preeti bhardwaj <127471577+Preeti684-bhardwaj@users.noreply.github.com>
2023-08-14 01:12:58 +0530

    Update newsRoutes.js

--- src/route/newsRoutes.js
+++ src/route/newsRoutes.js
@@ -3,8 +3,11 @@ const router=express.Router()
 const {createNews,getNews,getNewsById,getAllNews}=require('../controller/newsController')
 
 router.post('/createnews',createNews)
+
 router.get('/getnews',getNews)
+
 router.get('/getallnews',getAllNews)
+
 router.get('/getnews/:id',getNewsById)
 
 module.exports=router
